50848c6007aa3c2078d277904c314353ded2870b
HurayrahB <huray2007@gmail.com>
2026-05-03 21:12:46 -0400

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -14,9 +14,19 @@ int main() {
     std::string input;
     std::getline(std::cin, input);
 
+    if (input.substr(0, 5) == "type ") {
+      std::string cmdInput = input.substr(5);
+
+      if (cmdInput == "echo" || cmdInput == "exit" || cmdInput == "type") {
+        std::cout << cmdInput << " is a shell builtin" << std::endl;
+      } else {
+        std::cout << cmdInput << ": not found" << std::endl;
+      }
+    }
+
     if (input == "exit") {
       break;
-    } else if (input.substr(0, 4) == "echo") {
+    } else if (input.substr(0, 5) == "echo ") {
       std::cout << input.substr(5) << std::endl;
     } else {
       std::cout << input << ": command not found" << std::endl;\
